c0286796098457d7a16ee7739c418be82bcc59ad
DESKTOP-111JM1H\Kit Barry <kit.barry@sheridancollege.ca>
2026-01-14 08:07:36 -0500

    Reading for week 2 completed

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -2,25 +2,47 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    float speed = 0.01f;
+    public float speed = 0.01f;
+
+    Vector2 bottomLeft;
+    Vector2 topRight;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 newPos = transform.position;
-        newPos.x += speed;
-        transform.position = newPos;
+        //move the square
+        Vector3 newPosition = transform.position;
+        newPosition.x += speed * Time.deltaTime;
 
+        //check if the square is at the edge of the screen
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
-        if (screenPos.x < 0 || screenPos.x > Screen.width)
+
+        //test for the left edge
+        if (screenPos.x < 0)
         {
+            //set our position to be the world space position under pixel 0 in X
+
+            newPosition.x = bottomLeft.x;
+            //Y: multiply the speed by -1
             speed = speed * -1;
         }
+
+        //test for the right edge
+        if (screenPos.x > Screen.width)
+        {
+            //set our position to be the world space position under pixel Screen.width in X
+            newPosition.x = topRight.x;
+            //Y: multiply the speed by -1
+            speed = speed * -1;
+        }
+
+        transform.position = newPosition;
     }
 }
